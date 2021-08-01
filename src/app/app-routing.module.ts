8cce9535802c020bc2bb7b05b311a55f8b7ebf49
CharukaHS <charuka@protonmail.com>
2021-08-01 13:20:51 +0530

    Add article page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('src/app/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'article',
+    loadChildren: () => import('src/app/article/article.module').then(m => m.ArticleModule)
   }
 ];
 
